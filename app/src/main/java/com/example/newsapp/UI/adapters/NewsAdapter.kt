2cf6fd6f246cb04a5dbd11ea1ee3d61a6fd1a6b6
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-01 20:41:24 +0530

    Adding Adapters

---
+++ app/src/main/java/com/example/newsapp/UI/adapters/NewsAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.UI.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>(){
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+
+}
