8d109cf86d7905fa2029ea08677c0d15370aef85
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-01 14:16:20 +0530

    bottom Navigation setup complete

--- app/src/main/java/com/example/newsapp/UI/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/UI/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.newsapp.UI
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomnavigation.setupWithNavController(newsnavfragment.findNavController())
     }
 }
