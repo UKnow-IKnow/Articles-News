2e6a59016822a54ba5d723ab97802e4d5ef9380d
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-03 17:45:33 +0530

    Instantitae viewModel with fragments

--- app/src/main/java/com/example/newsapp/UI/fragments/BreakingNewsfragment.kt
+++ app/src/main/java/com/example/newsapp/UI/fragments/BreakingNewsfragment.kt
@@ -1,7 +1,18 @@
 package com.example.newsapp.UI.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.UI.MainActivity
+import com.example.newsapp.UI.NewsViewModel
 
 class BreakingNewsfragment: Fragment(R.layout.fragment_breakingnews) {
+
+    lateinit var  viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+    }
 }
--- app/src/main/java/com/example/newsapp/UI/fragments/SearchNewsfragment.kt
+++ app/src/main/java/com/example/newsapp/UI/fragments/SearchNewsfragment.kt
@@ -1,7 +1,18 @@
 package com.example.newsapp.UI.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
+import com.example.newsapp.UI.MainActivity
+import com.example.newsapp.UI.NewsViewModel
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search) {
+
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+    }
 }
